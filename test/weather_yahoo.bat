837613d33aeefcda31f4af7897e87cf2d57f1338
Tian Lin <bolitt@gmail.com>
2014-07-13 23:53:50 +0800

    change http to https

--- test/weather_yahoo.bat
+++ test/weather_yahoo.bat
@@ -1,4 +1,4 @@
 @echo off
 
 echo Should have bin\curl installed
-bin\curl "http://weather.yahoo.com/china/beijing/beijing-2151330/" | python demostreaming.py -t weather_yahoo.xml > [out]weather_yahoo.out.txt
+bin\curl "https://weather.yahoo.com/china/beijing/beijing-2151330/" | python demostreaming.py -t weather_yahoo.xml > [out]weather_yahoo.out.txt
